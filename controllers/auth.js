03641ce7a43c0b3a3024faecf0aa2ce5129a4b2f
MateichukNina <124276133+MateichukNina@users.noreply.github.com>
2023-11-23 02:35:23 +0200

    uploadAvatar

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,28 +1,27 @@
 const { User } = require("../models/user");
 const bcrypt = require("bcrypt");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
-const {registerSchema, loginSchema} = require("../models/user");
+const { registerSchema, loginSchema } = require("../models/user");
+const fs = require("fs/promises");
+const path = require("path");
 
-const {SECRET_KEY} = process.env;
+const { SECRET_KEY } = process.env;
 
 const { ctrlWrapper } = require("../helpers");
 
 const register = async (req, res, next) => {
   const { password, email } = req.body;
- 
 
   try {
     const user = await User.findOne({ email }).exec();
-     
-  const { error } = registerSchema.validate(req.body);
 
-  if (error) {
-    console.log('Validation Error:', error);
-    return res.status(400).json({ message: error.details[0].message });
-  }
+    const { error } = registerSchema.validate(req.body);
 
-  
+    if (error) {
+      console.log("Validation Error:", error);
+      return res.status(400).json({ message: error.details[0].message });
+    }
 
     if (user) res.status(409).send({ message: "Email in use" });
 
@@ -40,7 +39,7 @@ const register = async (req, res, next) => {
       },
     });
   } catch (error) {
-    console.log('Unexpected Error:', error);
+    console.log("Unexpected Error:", error);
     next({ status: 400, message: error.details[0].message });
   }
 };
@@ -48,32 +47,32 @@ const register = async (req, res, next) => {
 const login = async (req, res, next) => {
   try {
     const { email, password } = req.body;
-    
+
     const { error } = loginSchema.validate(req.body);
 
-  if (error) {
-    return res.status(400).json({ message: error.details[0].message });
-  }
-    
+    if (error) {
+      return res.status(400).json({ message: error.details[0].message });
+    }
 
-    // if (!email || !password) 
+    // if (!email || !password)
     // return res.status(400).json({ message: "missing required fields" });
 
     const user = await User.findOne({ email }).exec();
 
-    if (!user) return res.status(401).send({ message: "Email or password is wrong" });
+    if (!user)
+      return res.status(401).send({ message: "Email or password is wrong" });
 
     const isLogin = await bcrypt.compare(password, user.password);
 
-    if (!isLogin) return res.status(401).send({ message: "Email or password is wrong" });
+    if (!isLogin)
+      return res.status(401).send({ message: "Email or password is wrong" });
 
     const payload = {
       id: user._id,
     };
 
-   
     const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
-    await User.findByIdAndUpdate(user._id, {token});
+    await User.findByIdAndUpdate(user._id, { token });
 
     res.status(200).json({
       token,
@@ -81,28 +80,47 @@ const login = async (req, res, next) => {
     });
   } catch (error) {
     next({ status: 400, message: error.details[0].message });
-  
   }
 };
 
-const getCurrent = async (req, res) =>{
-  const {subscription ,email} = req.user;
+const getCurrent = async (req, res) => {
+  const { subscription, email } = req.user;
 
   res.json({
     subscription,
-    email
-  })
-}
+    email,
+  });
+};
 
-const logOut = async(req, res) =>{
-  const {_id} = req.user;
-  await User.findByIdAndUpdate(_id, {token: ""});
+const logOut = async (req, res) => {
+  const { _id } = req.user;
+  await User.findByIdAndUpdate(_id, { token: "" });
 
-  res.status(204).json({message: "No content"})
-}
+  res.status(204).json({ message: "No content" });
+};
 
-const uploadAvatar = async (req, res, next) =>{
-  res.send("Upload avatar!");
+const uploadAvatar = async (req, res, next) => {
+  try {
+    await fs.rename(
+      req.file.path,
+      path.join(__dirname, "..", "public/avatars", req.file.filename)
+    );
+
+    const updatedUser = await User.findByIdAndUpdate(
+      req.user.id,
+      {avatar: req.file.filename, avatarURL: `${req.file.filename}` },
+      { new: true }
+    ).exec();
+    console.log(updatedUser);
+
+    if (updatedUser === null) {
+      return res.status(404).send({ message: "User not found" });
+    }
+
+    res.status(200).send(updatedUser)
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = {
@@ -110,5 +128,5 @@ module.exports = {
   login: ctrlWrapper(login),
   getCurrent: ctrlWrapper(getCurrent),
   logOut: ctrlWrapper(logOut),
-  uploadAvatar: ctrlWrapper(uploadAvatar)
+  uploadAvatar: ctrlWrapper(uploadAvatar),
 };
--- helpers/upload.js
+++ helpers/upload.js
@@ -1,18 +1,25 @@
 const multer = require("multer");
-const path = require('path');
+const path = require("path");
+const crypto =require("crypto");
 
-const tmpDir = path.join(__dirname, "..", "tmp")
+const tmpDir = path.join(__dirname, "..", "tmp");
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) =>{
+  destination: (req, file, cb) => {
     console.log(file);
-    cb(null, tmpDir)
-  
-}})
+    cb(null, tmpDir);
+  },
+  filename: (req, file, cb)=>{
+    const extname = path.extname(file.originalname);
+    const basename = path.basename(file.originalname, extname);
+    const sufix = crypto.randomUUID();
+    console.log(basename);
+    cb(null, `${basename}-${sufix}${extname}`)
+  },
+});
 
 const upload = multer({
-  storage
-})
+  storage,
+});
 
 module.exports = upload;
-
